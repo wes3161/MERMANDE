fed4ba38bb764362cf2ff86f399c8bd34cc7a086
Wesley Smith <wes3161@gmail.com>
2019-08-09 00:05:19 -0400

    Update soundeffects.js

--- scripts/soundeffects.js
+++ scripts/soundeffects.js
@@ -1,3 +1,12 @@
-var audio = new Audio("sounds/largecash2.wav");
-audio.play();
+// Random Num
+var x = Math.floor((Math.random() * 100) + 1);
 
+// Num check
+if (x < 10) {
+  var audio = new Audio("sounds/largecash2.wav");
+
+} else {
+  var audio = new Audio("sounds/largecash.wav");
+}
+// play audio
+audio.play();

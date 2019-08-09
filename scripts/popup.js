2fe2444fa6dae4a246f6c1c478e751e27784bc77
Joe <RickyDrinksGasoline@users.noreply.github.com>
2019-08-09 00:52:02 -0400

    Update popup.js

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,12 @@
+var popupChance = Math.floor((Math.random() * 4) + 1);
+
+if (popupChance == 1) {
 i = 1;
 while (i<5) {
  confirm("Please Disable You're Adblocker!");
  i = i + 1;
-}
+};
+
+} else {
+
+}; 

c87d5f5c35872bb394fb87a5785e19fee50dd159
Joe <RickyDrinksGasoline@users.noreply.github.com>
2019-12-05 21:11:41 -0500

    Add files via upload

---
+++ scripts/seeker.js
@@ -0,0 +1,29 @@
+window.onload=function(){
+	var song = ["https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/463923036&color=%23ae41f4&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/615438117&color=%23ae41f4&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/533163915&color=%23ae41f4&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/566330946&color=%23ae41f4&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/650648105&color=%23ae41f4&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/462046557&color=%23ae41f4&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/532701396&color=%23ae41f4&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/650702015&color=%23ae41f4&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"];
+
+	document.getElementById("nextBtn").addEventListener("click", playNext);
+	document.getElementById("prevBtn").addEventListener("click", playPrev);
+
+	var pos = Math.floor((Math.random() * (song.length - 1)) + 1);
+ 	document.getElementById("scPlayer").src = song[pos];
+
+	function playNext(){
+		if(pos + 1 == song.length){
+			pos = 0;
+		} else {
+			pos++;
+		};
+
+		document.getElementById("scPlayer").src = song[pos];
+	};
+
+	function playPrev(){
+		if(pos - 1 < 0){
+			pos = song.length - 1;
+		} else {
+			pos--;
+		};
+
+		document.getElementById("scPlayer").src = song[pos];
+	};
+}

f783eaaa97f12e2dae3cceffc94c083b74856542
Wesley Smith <wes3161@gmail.com>
2019-12-07 14:39:22 -0500

    Update background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,4 @@
-var bgChance = Math.floor((Math.random() * 3) + 1);
+var bgChance = Math.floor((Math.random() * 4) + 1);
 
 if (bgChance <= 1) {
 	document.body.style.backgroundImage = "url('photos/site/purp.gif')";
@@ -6,4 +6,6 @@ if (bgChance <= 1) {
 	document.body.style.backgroundImage = "url('photos/site/gold.gif')";
 } else if(bgChance <= 3){
 	document.body.style.backgroundImage = "url('photos/site/fire.gif')";
+} else if(bgChance <= 4){
+	document.body.style.backgroundImage = "url('photos/site/giphy.webp')";	
 };

ddb1f50c1d7f98dd7fa687f819add12dc01b129d
Joe <RickyDrinksGasoline@users.noreply.github.com>
2019-12-10 15:46:04 -0500

    Update ad.js

--- scripts/ad.js
+++ scripts/ad.js
@@ -1,7 +1,14 @@
 window.addEventListener("load", ad);
 
 function ad(){
-var popupChance = Math.floor((Math.random() * 2) + 1);
+
+var pos1 = Math.floor(Math.random() * 6 + 1);
+var pos2 = Math.floor(Math.random() * 6 + 1);
+
+document.getElementById("ad1").src = "photos/ads/" + pos1 + ".jpg";
+document.getElementById("ad2").src = "photos/ads/" + pos2 + ".jpg";
+
+var popupChance = Math.floor((Math.random() * 4) + 1);
 
 var error = 0;
 
@@ -16,9 +23,10 @@ if (popupChance == 1) {
 			document.getElementById("headerImage").src = "photos/ads/disable3.png";
 			document.body.style.backgroundImage = "url('photos/ads/disable4.png')";
 		} else {
- 			i++;
+ 			i = i + 1;
 		};
 	};
+	popupChance = 0;
 }; 
 
 };

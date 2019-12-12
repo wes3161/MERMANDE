5f606efd630626b0797b232c04862c7a91cd52a4
Joe <RickyDrinksGasoline@users.noreply.github.com>
2019-12-11 22:13:02 -0500

    Update loader.js

--- scripts/loader.js
+++ scripts/loader.js
@@ -3,10 +3,9 @@ cursor doesn't work properly unless loaded into the document itself
 */
 
 var script = ["ad.js", "background.js", "seeker.js", "spinner.js"];
-var sName = {};
 
 for(i = 0; i < script.length; i++){
-	sName[script[i]] = document.createElement("script");
-	sName[script[i]].src = "scripts/" + script[i];
-	document.head.appendChild(sName[script[i]]);
+	var sName = document.createElement("script");
+	sName.src = "scripts/" + script[i];
+	document.head.appendChild(sName);
 };

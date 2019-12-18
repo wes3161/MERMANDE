2ea249693394b888661f50706a18a12b3bfad74e
Joe <RickyDrinksGasoline@users.noreply.github.com>
2019-12-17 21:13:59 -0500

    Update soundeffects.js

--- scripts/soundeffects.js
+++ scripts/soundeffects.js
@@ -1,3 +1,8 @@
+if(document.URL == "https://mermande.com/ad.html"){
+  var audio = new Audio("sounds/song.ogg");
+	audio.play();
+} else {
+
 // Random Num
 var x = Math.floor((Math.random() * 100) + 1);
 
@@ -15,4 +20,5 @@ var promise = audio.play();
 if (promise) {
     //Older browsers may not return a promise, according to the MDN website
     promise.catch(function(error) { console.error(error); });
+}
 }
